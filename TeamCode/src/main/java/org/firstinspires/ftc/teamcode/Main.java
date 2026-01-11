9ddd74456a5b0d214524437dab96a4b25b4be183
taigertails-svg <taigertails@gmail.com>
2026-01-11 10:00:20 +0200

    Improve variable names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -5,27 +5,27 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
 public class Main extends OpMode {
-    Drive drive = new Drive();
+    Drive Drive = new Drive();
 
-    double forward, strafe, rotate;
+    double Forward, Strafe, Rotate;
 
     // Initialize driving
     @Override
     public void init() {
-        drive.Init(hardwareMap);
+        Drive.Init(hardwareMap);
     }
 
+    // Initialize gamepad controls
     @Override
     public void loop() {
+        Forward = gamepad1.left_stick_y;
+        Strafe = -gamepad1.left_stick_x;
+        Rotate = -gamepad1.right_stick_x;
 
-        forward = gamepad1.left_stick_y;
-        strafe = -gamepad1.left_stick_x;
-        rotate = -gamepad1.right_stick_x;
-
-        drive.DriveFieldRelative(forward,strafe,rotate);
+        Drive.DriveFieldRelative(Forward,Strafe,Rotate);
 
         if (gamepad1.ps) {
-            drive.ResetIMU();
+            Drive.ResetIMU();
         }
     }
 }
