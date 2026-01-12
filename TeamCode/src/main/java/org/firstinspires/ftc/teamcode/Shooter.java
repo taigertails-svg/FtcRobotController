1b204c66329ee60d36c5bd5fa819419f3aba11fe
eitan25ch-create <taigertails@gmail.com>
2026-01-12 15:01:34 +0200

    Fix Shooter, Fix contributions.md.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -1,19 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.IMU;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class Shooter {
     private DcMotor ShooterMotor;
 
     public void Init(HardwareMap hwMap) {
         // Set variable
-        ShooterMotor = hwMap.get(DcMotor.class, "FR");
+        ShooterMotor = hwMap.get(DcMotor.class, "Shooter");
     }
 
     public void SetPower(double Speed) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/contributions.md
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/contributions.md
@@ -2,6 +2,6 @@
 
 a. No vibe coding (no AI)
 b. All variables should be pascal case (for example: int HelloWorld)
-c. All constants should be screaming snake case (for example: int HELLO_WORLD)
+c. All constants should be screaming snake case (for example: final int HELLO_WORLD)
 d. All code should compile without any errors
 e. Code should preferably not have any warnings (although there are some exceptions)
