df4528d93d8f98d8c82691db7c9eddfce12f6e8c
taigertails-svg <taigertails@gmail.com>
2026-01-13 10:28:34 +0200

    Add servos to the Shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -2,17 +2,27 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Shooter {
+    // Make variables
     private DcMotor ShooterMotor;
+    private Servo ServoLeft, ServoRight;
 
     public void Init(HardwareMap HwMap) {
-        // Set variable
+        // Set variables
         ShooterMotor = HwMap.get(DcMotor.class, "Shooter");
+        ServoLeft = HwMap.get(Servo.class, "SL");
+        ServoRight = HwMap.get(Servo.class, "SR");
+
+        // Set it to break because when testing it wastes time to wait for it to stop
+        ShooterMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     public void SetPower(double Speed) {
         // Set speed
         ShooterMotor.setPower(Speed);
+        ServoLeft.setPosition(Speed);
+        ServoRight.setPosition(Speed);
     }
 }
